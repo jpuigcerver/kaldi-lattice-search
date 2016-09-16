33caf6960aa2cc05af2dc2c4a7053a58ae7baa8a
Joan Puigcerver <joapuipe@gmail.com>
2016-09-16 19:42:48 +0200

    Improved utils. Created util for CTC queries

--- utils/Create_String_Query_FST.sh
+++ utils/Create_String_Query_FST.sh
@@ -5,21 +5,17 @@ export LC_NUMERIC=C;
 delimiters="";
 verbose=1000;
 usage="
-Usage: ${0##*/} [options] <symbols-table> <not-a-symbol> <strings-table> <fst-wspecifier>
+Usage: ${0##*/} [options] <symbols-table> <strings-table> <fst-wspecifier>
 
 Description:
   Formally, for each input string (s1 s2 ... sN) this util creates a DFA (in
-  the form of a FST) representing the language(*):
+  the form of a FST) representing the language:
 
-  (\Sigma^* [\B* <delimiters>+])? \B* s1+ \B* s2+ ... \B* sN+ ([\B* <delimiters>+] \Sigma^*)?
+  (\Sigma^* [<delimiters>])? s1 s2 ... sN  ([<delimiters>] \Sigma^*)?
 
   The alphabet of the language (\Sigma) is obtained from the symbols table
   <symbols-table> (each line maps a symbol from the alphabet to an integer
   value, i.e. Kaldi symbols tables).
-
-  The <not-a-symbol> (\B) can be emitted in between of any two regular symbols,
-  except in the case of two equal symbols from the input string, in which case
-  the emission of <not-a-symbol> is mandatory (see the CTC definition).
 
   The input strings are read from the <strings-table> file (or stdin). Each
   line represents a string, identified by the first token, formed by the
@@ -50,7 +46,7 @@ while [ "${1:0:2}" = "--" ]; do
 	    echo "Unknown option: \"$1\" for ${0##*/}" >&2 && exit 1;
     esac;
 done;
-[ $# -ne 4 ] && echo "$usage" >&2 && exit 1;
+[ $# -ne 3 ] && echo "$usage" >&2 && exit 1;
 
 function check_exec() {
     any_missing=0;
@@ -66,7 +62,7 @@ function check_exec() {
 check_exec gawk fstcompile fstcopy fstdeterminizestar fstminimizeencoded \
     fstarcsort fstprint || exit 1;
 
-gawk -v STF="$1" -v DLM="$delimiters " -v BLANK="$2" -v verbose="$verbose" '
+gawk -v STF="$1" -v DLM="$delimiters " -v verbose="$verbose" '
 BEGIN{
   max_id=0; numq=0;
   while ((getline < STF) > 0) {
@@ -79,13 +75,9 @@ BEGIN{
       print "SYMBOL \"" DELIMITERS[s] "\" NOT FOUND IN THE SYMBOLS TABLE!" > "/dev/stderr"; exit 1;
     }
   }
-  if (!(BLANK in SYMBS)) {
-    print "SYMBOL \"" BLANK "\" NOT FOUND IN THE SYMBOLS TABLE!" > "/dev/stderr"; exit 1;
-  }
-  BLANK = SYMBS[BLANK];
   cmd="(fstcompile --acceptor | fstdeterminizestar --use-log=false | fstminimizeencoded | fstarcsort --sort_type=ilabel | fstprint) 2> /dev/null";
   ## Uncomment this to debug the automaton
-  cmd="(fstcompile --acceptor --keep_state_numbering | fstprint) 2> /dev/null";
+  ##cmd="(fstcompile --acceptor --keep_state_numbering | fstprint) 2> /dev/null";
 }NF > 0{
   ++numq;
   if (numq % verbose == 0) {
@@ -93,6 +85,7 @@ BEGIN{
   }
   print $1;
   if (NF > 1) {
+    ## The string is not empty
     N = NF - 1;
     missing_symbols = 0;
     for (i=2; i <= NF && !missing_symbols; ++i) {
@@ -102,59 +95,27 @@ BEGIN{
       }
     }
     if (!missing_symbols) {
-      W_BEG = 3 * ND + 2;            # State where the keyword begins
-      W_END = 3 * ND + N * 2 + 2;    # State where the keyword ends
-      FINAL = 6 * ND + N * 2 + 4;    # Final state
-      # Line can start with: (\Sigma^* [<delimiters>])?
       print 0, 1, 0 | cmd;
-      print 0, W_BEG, 0 | cmd;
+      print 0, 2, 0 | cmd;
       for (s in SYMBS) { print 1, 1, SYMBS[s] | cmd; }
       if (ND > 0) {
-        for (i in DELIMITERS) {
-          s = SYMBS[DELIMITERS[i]];
-          s_b = 2 + (i - 1) * 3;
-          print 1, s_b, 0               | cmd;
-          print s_b, s_b + 1, BLANK     | cmd;
-          print s_b, s_b + 2, s         | cmd;
-          print s_b + 1, s_b + 1, BLANK | cmd;
-          print s_b + 1, s_b + 2, s     | cmd;
-          print s_b + 2, s_b + 2, s     | cmd;
-          print s_b + 2, W_BEG, 0       | cmd;
-        }
+        for (s in DELIMITERS) { print 1, 2, SYMBS[DELIMITERS[s]] | cmd; }
       } else {
-        print 1, W_BEG, 0 | cmd;
+        print 1, 2, 0 | cmd;
       }
-      # Line must contain the keyword: k e y w o r d
-      ps = "";
-      for (i = 2; i <= NF; ++i) {
-        s = SYMBS[$i];
-        s_b = W_BEG + (i - 2) * 2;
-        print s_b, s_b + 1, BLANK                     | cmd;
-        if (i > 0 && ps != s) { print s_b, s_b + 2, s | cmd; }
-        print s_b + 1, s_b + 1, BLANK                 | cmd;
-        print s_b + 1, s_b + 2, s                     | cmd;
-        print s_b + 2, s_b + 2, s                     | cmd;
-        ps = s;
+      for (i=2; i <= NF; ++i) {
+        s=SYMBS[$i];
+        print i, i + 1, s | cmd;
       }
       if (ND > 0) {
-        for (i in DELIMITERS) {
-          s = SYMBS[DELIMITERS[i]];
-          s_b = W_END + 1 + (i - 1) * 3;
-          print W_END, s_b, 0           | cmd;
-          print s_b, s_b + 1, BLANK     | cmd;
-          print s_b, s_b + 2, s         | cmd;
-          print s_b + 1, s_b + 1, BLANK | cmd;
-          print s_b + 1, s_b + 2, s     | cmd;
-          print s_b + 2, s_b + 2, s     | cmd;
-          print s_b + 2, FINAL - 1, 0   | cmd;
-        }
+        for (s in DELIMITERS) { print N + 2, N + 3, SYMBS[DELIMITERS[s]] | cmd; }
       } else {
-        print W_END, FINAL - 1, 0 | cmd;
+        print N + 2, N + 3, 0 | cmd;
       }
-      print W_END, FINAL, 0 | cmd;
-      for (s in SYMBS) { print FINAL - 1, FINAL - 1, SYMBS[s] | cmd; }
-      print FINAL - 1, FINAL, 0 | cmd;
-      print FINAL | cmd;
+      print N + 2, N + 4, 0 | cmd;
+      for (s in SYMBS) { print N + 3, N + 3, SYMBS[s] | cmd; }
+      print N + 3, N + 4, 0 | cmd;
+      print N + 4 | cmd;
     }
   } else {
     ## The string is not empty, accept \Sigma^*
@@ -163,4 +124,4 @@ BEGIN{
   }
   close(cmd);
   print "";
-}' "$3" | fstcopy ark:- "$4";
+}' "$2" | fstcopy ark:- "$3";
